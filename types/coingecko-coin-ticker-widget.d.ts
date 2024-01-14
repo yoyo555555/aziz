2543ae888d2eb92b8c3aa6d2c04088be8487dcb9
Jhon flow <jhonflow@gmail.com>
2024-01-14 11:26:34 -0800

    b''h ca va marcher

--- types/coingecko-coin-ticker-widget.d.ts
+++ types/coingecko-coin-ticker-widget.d.ts
@@ -1,7 +1,15 @@
-// types/coingecko-coin-ticker-widget.d.ts
+// coingecko-coin-ticker-widget.d.ts
 
 declare module 'coingecko-coin-ticker-widget' {
-    const CoinGeckoCoinTickerWidget: React.ComponentType<any>;
+    interface CoinGeckoCoinTickerWidgetProps {
+      coinId: string;
+      currency: string;
+      locale?: string;
+      backgroundColor?: string;
+    }
+  
+    class CoinGeckoCoinTickerWidget extends React.Component<CoinGeckoCoinTickerWidgetProps> {}
+  
     export default CoinGeckoCoinTickerWidget;
   }
   
