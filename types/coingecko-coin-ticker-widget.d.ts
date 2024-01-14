9578d0ccb63984e28c5684fc3aa53c71d67d9b0f
Jhon flow <jhonflow@gmail.com>
2024-01-14 11:14:39 -0800

    26 26 26 b''h

---
+++ types/coingecko-coin-ticker-widget.d.ts
@@ -0,0 +1,7 @@
+// types/coingecko-coin-ticker-widget.d.ts
+
+declare module 'coingecko-coin-ticker-widget' {
+    const CoinGeckoCoinTickerWidget: React.ComponentType<any>;
+    export default CoinGeckoCoinTickerWidget;
+  }
+  
