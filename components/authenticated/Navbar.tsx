5bdef66f32f4723dbaffd9dd2a558fefb9fece94
Jhon flow <jhonflow@gmail.com>
2024-01-14 13:29:25 -0800

    suceeeee

--- components/authenticated/Navbar.tsx
+++ components/authenticated/Navbar.tsx
@@ -13,10 +13,7 @@ import { GiReceiveMoney } from "react-icons/gi";
 import { BsPeopleFill } from "react-icons/bs";
 import { MdContactPhone } from "react-icons/md";
 
-
-import "./Navbar.css"
-
-
+import "./Navbar.css";
 
 const Navbar = () => {
   const pathName = usePathname();
@@ -33,10 +30,6 @@ const Navbar = () => {
   const [moneyHover, setMoneyHover] = useState(false);
   const [walletHover, setWalletHover] = useState(false);
   const [accountHover, setAccountHover] = useState(false);
-
-
-  
-   
 
   const getColor = (condition: boolean, hoverState: boolean) => {
     if (condition) {
@@ -58,162 +51,87 @@ const Navbar = () => {
     }
   };
 
-
-
-
-
   return (
-    <nav
-    id="navbar"
-      className={`shadow-lg flex justify-between 
+    <>
+      <nav
+        id="navbar"
+        className={`shadow-lg flex justify-between 
     items-center px-4 sm:px-10 fixed 
     w-full py-3 z-30
      top-0 transition-colors duration-500
     ${mode === "light" ? "bg-white" : "bg-[#121212]"}`}
-    >
-    <div id="logo">
-      <Logo />
-      </div>
-      {/* <img src={imageToAdd} alt="Image" width={100} height={50} /> */}
-
-
-      <div className="sm:flex items-center gap-10 hidden" id="itemNav">
-        <div
-          onMouseEnter={() => setHomeHover(true)}
-          onMouseLeave={() => setHomeHover(false)}
-          // style={{
-          //   color: getColor(pathName.startsWith("/home"), homeHover),
-          // }}
-          onClick={() => router.push("/home")}
-          className={`flex cursor-pointer 
-        text-lg items-center gap-1 font-semibold`}
-        >
-          <MdDashboard />
-          Dashboard </div>
-   
-       
-
-    
-
-
-<div      
-          onClick={() => router.push("/home/invest-and-earn")}
-          className={`flex cursor-pointer 
-        text-lg items-center gap-1 font-semibold`}
-        >
-     <GiReceiveMoney />
-          Pool </div>
-          
-<<<<<<< HEAD
-<div
-          onMouseEnter={() => setHomeHover(true)}
-          onMouseLeave={() => setHomeHover(false)}
-          // style={{
-          //   color: getColor(pathName.startsWith("/home/referrals"), homeHover),
-          // }}
-          onClick={() => router.push("/home/referrals")}
-          className={`flex cursor-pointer 
-        text-lg items-center gap-1 font-semibold`}
-        >
-          <BsPeopleFill/>
-          Referrals </div>
-         
-
-          {/* <div
-          onMouseEnter={() => setHomeHover(true)}
-          onMouseLeave={() => setHomeHover(false)}
-          // style={{
-          //   color: getColor(pathName.startsWith("/home/invest-and-earn"), homeHover),
-          // }}
-          onClick={() => router.push("/home/invest-and-earn")}
-          className={`flex cursor-pointer 
-        text-lg items-center gap-1 font-semibold`}
-        >
-          Crypto </div> */}
-
-        <div
-          onMouseEnter={() => setCardHover(true)}
-          onMouseLeave={() => setCardHover(false)}
-          // style={{
-          //   color: getColor(pathName.startsWith("/card"), cardHover),
-          // }}
-=======
-
-
-         
-
-        <div
-          onMouseEnter={() => setWalletHover(true)}
-          onMouseLeave={() => setWalletHover(false)}
-          style={{
-            color: getColor(pathName.startsWith("/card"), walletHover),
-          }}
->>>>>>> 0fe0155a7c52bf766ab763b12ab0bab438679373
-          onClick={() => router.push("/card")}
-          className={`flex cursor-pointer 
-        text-lg items-center gap-1 font-semibold`}
-        >
-<<<<<<< HEAD
-          <MdContactPhone />
-          Contact
-          {/* <FaCreditCard /> */}
+      >
+        <div id="logo">
+          <Logo />
         </div>
-        {/* <div className="sm:flex items-center gap-10 hidden"> */}
-          {/* <div
+
+        <div className="sm:flex items-center gap-10 hidden" id="itemNav">
+          <div
             onMouseEnter={() => setHomeHover(true)}
             onMouseLeave={() => setHomeHover(false)}
-            style={{
-              color: getColor(pathName.startsWith("/about"), homeHover),
-            }}
-            onClick={() => router.push("/about")}
+            onClick={() => router.push("/home")}
             className={`flex cursor-pointer 
         text-lg items-center gap-1 font-semibold`}
           >
-            A propos
-            {/* <FaHome /> */}
-          {/* </div>{" "} */}
-        {/* </div> */} 
-=======
-          Wallet
->>>>>>> 0fe0155a7c52bf766ab763b12ab0bab438679373
-      
-        </div>
-        
+            <MdDashboard />
+            Dashboard
+          </div>
 
-      </div>
-
-
-
-
-      <div className="flex items-center" id="account">
-        <ThemeToggle />
-
-        <div
-          onMouseEnter={() => setAccountHover(true)}
-          onMouseLeave={() => setAccountHover(false)}
-          style={{
-            color: getColor(pathName.startsWith("/account"), accountHover),
-          }}
-          onClick={() => router.push("/account")}
-          className={`flex cursor-pointer
+          <div
+            onClick={() => router.push("/home/invest-and-earn")}
+            className={`flex cursor-pointer 
         text-lg items-center gap-1 font-semibold`}
-        >
-          <span className="hidden sm:block">Account</span>
-          <IoMdPerson size={22} />
+          >
+            <GiReceiveMoney />
+            Pool
+          </div>
+
+          <div
+            onMouseEnter={() => setHomeHover(true)}
+            onMouseLeave={() => setHomeHover(false)}
+            onClick={() => router.push("/home/referrals")}
+            className={`flex cursor-pointer 
+        text-lg items-center gap-1 font-semibold`}
+          >
+            <BsPeopleFill />
+            Referrals
+          </div>
+
+          <div
+            onClick={() => router.push("/card")}
+            className={`flex cursor-pointer 
+        text-lg items-center gap-1 font-semibold`}
+          >
+            <MdContactPhone />
+            Contact
+          </div>
         </div>
 
-        <IoMdMenu
-          onClick={onOpen}
-          size={24}
-          className={`sm:hidden cursor-pointer
+        <div className="flex items-center" id="account">
+          <ThemeToggle />
+
+          <div
+            onMouseEnter={() => setAccountHover(true)}
+            onMouseLeave={() => setAccountHover(false)}
+            style={{
+              color: getColor(pathName.startsWith("/account"), accountHover),
+            }}
+            onClick={() => router.push("/account")}
+            className={`flex cursor-pointer
+        text-lg items-center gap-1 font-semibold`}
+          >
+            <span className="hidden sm:block">Account</span>
+            <IoMdPerson size={22} />
+          </div>
+          <IoMdMenu
+            onClick={onOpen}
+            size={24}
+            className={`sm:hidden cursor-pointer
           ${mode === "light" ? "text-slate-700" : "text-white"}`}
-        />
-      </div>
-
-
-
-      
-    </nav>
+          />
+        </div>
+      </nav>
+    </>
   );
 };
 
--- components/authenticated/crypto-wallet/index.tsx
+++ components/authenticated/crypto-wallet/index.tsx
@@ -1,60 +1,28 @@
-"use client";
-import React, { useEffect } from "react";
-import "./style.css"; // Chemin vers votre feuille de style CSS
-import { useRouter } from "next/navigation";
-import Button from "@/components/Button";
+'use client'
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
 
-const CoinGeckoWidget = () => {
-  const router = useRouter();
+const CoinTickerWidget = () => {
   useEffect(() => {
-    // Charge le script du widget Coingecko Coin Ticker dès que le composant est monté
-    const script = document.createElement("script");
-    script.src = "https://widgets.coingecko.com/coingecko-coin-ticker-widget.js";
+    const script = document.createElement('script');
+    script.src = 'https://widgets.coingecko.com/coingecko-coin-ticker-widget.js';
     script.async = true;
     document.body.appendChild(script);
 
-    // Nettoie le script lorsque le composant est démonté
     return () => {
       document.body.removeChild(script);
     };
   }, []);
 
   return (
-    <div style={{ display: "flex", gap: "20px", justifyContent: "center", paddingTop: "50px" }}>
-      {/* Utilisez le premier widget Coingecko Coin Ticker après que le script est chargé */}
-      <div className="widget-container1">
-        <coingecko-coin-ticker-widget
-          coin-id="bitcoin"
-          currency="usd"
-          locale="fr"
-          background-color="#ffffff"
-        ></coingecko-coin-ticker-widget>
-        <Button onClick={() => router.push("/add-money")} label={"Make a deposit"} />
-      </div>
-
-      {/* Utilisez le deuxième widget Coingecko Coin Ticker après que le script est chargé */}
-      <div className="widget-container2">
-        <coingecko-coin-ticker-widget
-          coin-id="ethereum"
-          currency="usd"
-          locale="fr"
-          background-color="#ffffff"
-        ></coingecko-coin-ticker-widget>
-          <Button onClick={() => router.push("/add-money")} label={"Make a deposit"} />
-      </div>
-
-      {/* Utilisez le troisième widget Coingecko Coin Ticker après que le script est chargé */}
-      <div className="widget-container3">
-        <coingecko-coin-ticker-widget
-          coin-id="tether"
-          currency="usd"
-          locale="fr"
-        ></coingecko-coin-ticker-widget>
-          <Button onClick={() => router.push("/add-money")} label={"Make a deposit"} />
-      </div>
+    <div>
+      <Helmet>
+        <script src="https://widgets.coingecko.com/coingecko-coin-ticker-widget.js" async />
+      </Helmet>
+      <coingecko-coin-ticker-widget coin-id="bitcoin" currency="usd" locale="en"></coingecko-coin-ticker-widget>
     </div>
   );
 };
 
-export default CoinGeckoWidget;
+export default CoinTickerWidget;
